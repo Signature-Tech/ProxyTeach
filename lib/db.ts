9d7cd8d003eea89a66bd4060a88fffdf6fdc2d01
Tariq Hassan <thefk.fakenking@gmail.com>
2024-10-04 23:46:49 +0600

    Auth Initiated

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/xata-http';
 import { XataClient } from '@/xata'; // Generated client
 
-const xata = new XataClient({ apiKey: process.env.XATA_API_KEY, branch: process.env.XATA_BRANCH });
+const xata = new XataClient({ apiKey: "xau_Fb2Jvg3g3zqmvsMFgf8taxvPVGCtIXz75", branch: "main" });
 export const db = drizzle(xata);

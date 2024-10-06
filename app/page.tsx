ef5118ef0f58d4bfa6176764f6ffc6814ad5879a
Tariq Hassan <thefk.fakenking@gmail.com>
2024-10-06 22:54:27 +0600

    Home Page Initiated

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,37 @@
+import { integer, pgTable, text } from "drizzle-orm/pg-core";
 import NavbarComponent from "@/components/Navbar";
 import Teacher from "@/components/CardLayout";
 import logo from "@/images/logo.png";
+import { db } from "@/lib/db";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  
-  return (
-    <main>
-      <NavbarComponent />
-      <div className="flex m-5 p-5 flex-wrap">
-        <Teacher name={"Teacher A"} picture={logo} />
-        <Teacher name={"Teacher B"} picture={logo} />
-        <Teacher name={"Teacher C"} picture={logo} />
-        <Teacher name={"Teacher D"} picture={logo} />
-        <Teacher name={"Teacher E"} picture={logo} />
-        <Teacher name={"Teacher F"} picture={logo} />
-        <Teacher name={"Teacher G"} picture={logo} />
-      </div>
-    </main>
-  );
+
+  const session = await auth();
+
+  const teachers = pgTable('teachers', {
+    id: integer('teach_id'),
+    name: text('name'),
+    number: integer('number'),
+  })
+
+  const response = await db.select().from(teachers);
+
+  if (session) {
+    return (
+      <main>
+        <NavbarComponent />
+        <div className="flex m-5 p-5 flex-wrap">
+          {response.map((teacher) => (
+            <Teacher key={teacher.id} name={teacher.name as string} picture={logo} />
+          ))}
+        </div>
+      </main>
+    );
+  }
+
+  else {
+    redirect('/api/auth/signin')
+  }
 }
